2049e078b6943f797a94bf4d485ef98441b4217e
Philip Fulcher <philipfulcher@Philips-MacBook-Air.local>
2022-11-30 22:04:50 -0700

    chore: add archive generator

---
+++ libs/plugin/src/generators/archive-days/generator.ts
@@ -0,0 +1,38 @@
+import {
+  getProjects,
+  readProjectConfiguration,
+  joinPathFragments,
+  Tree,
+  updateProjectConfiguration, logger,
+} from '@nrwl/devkit';
+import {ArchiveDaysGeneratorSchema} from './schema';
+import {moveGenerator} from "@nrwl/workspace";
+
+
+export default async function (tree: Tree, options: ArchiveDaysGeneratorSchema) {
+  const {year} = options;
+  const projects = getProjects(tree);
+  for (const [projectName, project] of projects) {
+    logger.log(`Checking project ${projectName}`);
+
+    if (!projectName.startsWith("day") || project.tags.filter(tag => tag.startsWith('year')).length > 0) return;
+
+    console.log(`Archiving ${projectName}`);
+    const destination = joinPathFragments(year.toString(), project.name);
+    const importPath = `@advent-of-code/${year}/${project.name}`;
+
+    updateProjectConfiguration(tree, project.name, {
+      ...project,
+      tags: [`year:${year}`]
+    });
+
+    await moveGenerator(tree, {
+      projectName: project.name,
+      destination,
+      updateImportPath: true,
+      importPath,
+      skipFormat: true
+    });
+  }
+
+}
---
+++ libs/plugin/src/generators/archive-days/schema.d.ts
@@ -0,0 +1,4 @@
+export interface ArchiveDaysGeneratorSchema {
+    year: number;
+
+}
