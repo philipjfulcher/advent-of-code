fc612e7f47fc017a04df6b2657703f417e1ec7a4
Philip Fulcher <philip@nrwl.io>
2022-12-03 16:01:20 -0700

    year 2022, day 3 complete (#5)
    Co-authored-by: Philip Fulcher <philipfulcher@Philips-MacBook-Air.local>

---
+++ libs/year-2022/day-03/README.md
@@ -0,0 +1,11 @@
+# year-2022-day-03
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test year-2022-day-03` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint year-2022-day-03` to execute the lint via [ESLint](https://eslint.org/).
---
+++ libs/year-2022/day-03/src/lib/02.ts
@@ -0,0 +1,40 @@
+import { createInterface } from 'readline';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import {
+  calculatePriority,
+  decodeContents,
+  findSharedContents,
+} from './shared';
+
+export async function calculateAnswer(fileName: string) {
+  const promise = new Promise((resolve) => {
+    console.log(`Reading from ${join(__dirname, fileName)}`);
+
+    const rl = createInterface({
+      input: createReadStream(join(__dirname, fileName)),
+    });
+
+    let total = 0;
+    let currentGroup: string[] = [];
+
+    rl.on('line', (line) => {
+      currentGroup.push(line);
+
+      if (currentGroup.length === 3) {
+        total += calculatePriority(findSharedContents(currentGroup));
+        currentGroup = [];
+      }
+    });
+
+    rl.on('close', () => {
+      const answer = total;
+
+      console.log(`The answer is ${answer}`);
+
+      resolve(answer);
+    });
+  });
+
+  return promise;
+}
