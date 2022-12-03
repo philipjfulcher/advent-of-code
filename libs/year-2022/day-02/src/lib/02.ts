331f26f7aa566529a887c933d50d333e264fc041
Philip Fulcher <philip@nrwl.io>
2022-12-02 22:32:54 -0700

    year 2022, day 02 complete (#4)
    Co-authored-by: Philip Fulcher <philipfulcher@Philips-MacBook-Air.local>

---
+++ libs/year-2022/day-02/src/lib/02.ts
@@ -0,0 +1,36 @@
+import { createInterface } from 'readline';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import {  playGame, calculateGameScore, decrypt, decryptExercise2, getDesiredGamestate } from './interfaces'
+export async function calculateAnswer(fileName: string) {
+  const promise = new Promise((resolve) => {
+    console.log(`Reading from ${join(__dirname, fileName)}`);
+
+    const rl = createInterface({
+      input: createReadStream(join(__dirname, fileName)),
+    });
+
+    let totalScore = 0;
+
+    rl.on('line', (line) => {
+      const codes = line.split(' ');
+      const opponent = decrypt[codes[0]];
+      const desiredGamestate = decryptExercise2[codes[1]];
+      const player = getDesiredGamestate(opponent,desiredGamestate);
+
+      const gamestate = playGame(player,opponent);
+      const score = calculateGameScore(player,gamestate);
+      totalScore += calculateGameScore(player,gamestate);
+    });
+
+    rl.on('close', () => {
+      const answer = totalScore;
+
+      console.log(`The answer is ${answer}`);
+
+      resolve(answer);
+    });
+  });
+
+  return promise;
+}
