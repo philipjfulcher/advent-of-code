dbd49b291c271fb5af677b60ad450e0dc6d80364
Philip Fulcher <philipfulcher@Philips-MacBook-Air.local>
2022-12-06 17:06:18 -0700

    year 2022, day 06 complete

---
+++ libs/year-2022/day-06/src/lib/shared.ts
@@ -0,0 +1,14 @@
+export function findStartOfPacketMarker(
+  input: string,
+  markerLength: number = 4
+) {
+  let endOfMarkerIndex = markerLength - 1;
+  for (let index = 0; index < input.length; index++) {
+    endOfMarkerIndex++;
+
+    const testString = input.slice(index, endOfMarkerIndex);
+    if (testString.length === new Set(testString).size) break;
+  }
+
+  return endOfMarkerIndex;
+}
