37e028fbb9e2f330dee7682a9468ac2a39f84296
Philip Fulcher <philipfulcher@Philips-MacBook-Air.local>
2023-12-08 22:19:20 -0700

    solved 2023 day 07

---
+++ libs/year-2023/day-07/src/lib/02.ts
@@ -0,0 +1,36 @@
+import { createInterface } from 'readline';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { rankHands } from './cards';
+
+export async function calculateAnswer(fileName: string) {
+  const promise = new Promise((resolve) => {
+    console.log(`Reading from ${join(__dirname, fileName)}`);
+
+    const rl = createInterface({
+      input: createReadStream(join(__dirname, fileName)),
+    });
+
+    const hands = new Map<string, number>();
+
+    rl.on('line', (line) => {
+      const [hand, bid] = line.split(' ');
+      hands.set(hand, parseInt(bid, 10));
+    });
+
+    rl.on('close', () => {
+      const rankedHands = rankHands([...hands.keys()], true);
+      rankedHands.reverse();
+
+      const answer = rankedHands.reduce((acc, cur, index) => {
+        return acc + hands.get(cur) * (index + 1);
+      }, 0);
+
+      console.log(`The answer is ${answer}`);
+
+      resolve(answer);
+    });
+  });
+
+  return promise;
+}
