e3a1ff5665259d2f3f52c6a6fa258faf99760ea0
Philip Fulcher <philip@headovertarheels.com>
2026-05-12 15:13:45 -0400

    2024 day 07? (#33)
    * 2024 day 07?
    
    * fixing tests
    
    * formatting

--- libs/year-2024/day-06/src/lib/01.ts
+++ libs/year-2024/day-06/src/lib/01.ts
@@ -1,7 +1,7 @@
 import { createInterface } from 'readline';
 import { createReadStream } from 'fs';
 import { join } from 'path';
-import {createMatrix} from "@advent-of-code/util-arrays";
+import { createMatrix } from '@advent-of-code/util-arrays';
 
 export async function calculateAnswer(fileName: string) {
   const promise = new Promise((resolve) => {
@@ -20,31 +20,37 @@ export async function calculateAnswer(fileName: string) {
     rl.on('close', () => {
       const map = createMatrix<string>(mapData);
       let now = Date.now();
-      let currentCell = map.filter((row,col,value) => value === '^')[0];
+      let currentCell = map.filter((row, col, value) => value === '^')[0];
 
-      console.log(`Took ${Date.now()-now}ms to find current cell`);
+      console.log(`Took ${Date.now() - now}ms to find current cell`);
       now = Date.now();
 
-      const rotationOrder: ('n' | 's' | 'e' |'w')[] = ['n','e','s','w']
+      const rotationOrder: ('n' | 's' | 'e' | 'w')[] = ['n', 'e', 's', 'w'];
       let currentDirection = 0;
 
-      while(currentCell !== null) {
-        const neighbors = map.getNeighborsForCell(currentCell.row,currentCell.col);
-        const nextCell = neighbors.find(neighbor => neighbor.direction === rotationOrder[currentDirection % 4]);
-        if(nextCell === undefined) {
-          map.setCellValue(currentCell.row,currentCell.col,'X');
+      while (currentCell !== null) {
+        const neighbors = map.getNeighborsForCell(
+          currentCell.row,
+          currentCell.col
+        );
+        const nextCell = neighbors.find(
+          (neighbor) =>
+            neighbor.direction === rotationOrder[currentDirection % 4]
+        );
+        if (nextCell === undefined) {
+          map.setCellValue(currentCell.row, currentCell.col, 'X');
           currentCell = null;
-        } else if(nextCell?.value !== '#') {
-          map.setCellValue(nextCell.row,nextCell.col,'^');
-          map.setCellValue(currentCell.row,currentCell.col,'X');
+        } else if (nextCell?.value !== '#') {
+          map.setCellValue(nextCell.row, nextCell.col, '^');
+          map.setCellValue(currentCell.row, currentCell.col, 'X');
           currentCell = nextCell;
         } else {
           currentDirection++;
         }
       }
 
-      const answer = map.filter((row,col,value) => value === 'X').length;
-      console.log(`Took ${Date.now() - now}ms to solve`)
+      const answer = map.filter((row, col, value) => value === 'X').length;
+      console.log(`Took ${Date.now() - now}ms to solve`);
       console.log(`The answer is ${answer}`);
 
       resolve(answer);
---
+++ libs/year-2024/day-07/src/lib/01.spec.ts
@@ -0,0 +1,24 @@
+import { calculateAnswer, solveEquation } from './01';
+
+describe.skip('day 07, exercise 01', () => {
+  it('should calculate the answer in test data', async () => {
+    const results = await calculateAnswer('test-input.txt');
+
+    expect(results).toEqual(3749);
+  });
+
+  it('should calculate the answer', async () => {
+    const results = await calculateAnswer('input.txt');
+
+    expect(results).toEqual(0);
+  });
+});
+
+describe.skip('solveEquation', () => {
+  it('should calculate equation in order', () => {
+    expect(solveEquation([10, '*', 19])).toEqual(190);
+    expect(solveEquation([81, '+', 40, '*', 27])).toEqual(3267);
+    expect(solveEquation([81, '*', 40, '+', 27])).toEqual(3267);
+    expect(solveEquation([11, '+', 6, '*', 16, '+', 20])).toEqual(292);
+  });
+});
